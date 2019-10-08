0764d44b3578cac97a1fc1446ccba6c1c71a38b5
Enrique Gomes <enriquerique@hotmail.com>
2019-10-07 21:09:39 -0300

    Modal de confirmação de exclusão em colaborador

--- adm/js/main.js
+++ adm/js/main.js
@@ -1,12 +1,13 @@
 $(() => {
+
     window.location.hash = ""
 
-    window.onpopstate = function(event){
-        
+    window.onpopstate = function (event) {
+
         let paginaDestino = window.location.hash.replace(/#/, '')
 
         this.setConteudoPaginaClicada(paginaDestino)
-        
+
     }
 
     window.location.hash = "colaborador"
@@ -17,27 +18,66 @@ $(() => {
         $('#modal').iziModal('open');
     });
 
-//EDITAR COLABORADOR
+    //EDITAR COLABORADOR
 
-    $(document).on('click', ".editar-colaborador", function(event){
+    $(document).on('click', ".editar-colaborador", function (event) {
         let iconeClicado = event.currentTarget
         let idColaborador = $(iconeClicado).data("id")
         let colaboradorObjeto = {
-            nome: $(iconeClicado).closest('#colaborador-'+idColaborador).children('td')[0].innerHTML,
-            email: $(iconeClicado).closest('#colaborador-'+idColaborador).children('td')[1].innerHTML,
-            profissao: $(iconeClicado).closest('#colaborador-'+idColaborador).children('td')[2].innerHTML
+            nome: $(iconeClicado).closest('#colaborador-' + idColaborador).children('td')[0].innerHTML,
+            email: $(iconeClicado).closest('#colaborador-' + idColaborador).children('td')[1].innerHTML,
+            profissao: $(iconeClicado).closest('#colaborador-' + idColaborador).children('td')[2].innerHTML
         }
         $("#modal-editar").iziModal('open')
         $("#modal-editar").find('input#txbNome').val(colaboradorObjeto.nome)
         $("#modal-editar").find('input#txbEmail').val(colaboradorObjeto.email)
         $("#modal-editar").find('select#cbbCargo').val(colaboradorObjeto.profissao)
-        
-    });
-})
 
-function setConteudoPaginaClicada(paginaASerCarregada){
+    });
+
+    //DELETAR COLABORADOR
+
+    $(document).on("click", ".deletar-colaborador", function (event) {
+
+        let iconeClicado = event.currentTarget;
+        let idColaborador = $(iconeClicado).data("id");
+
+        $.confirm({
+            title: 'Excluir',
+            content: 'Deseja realmente excluir este item?',
+            icon: "fa fa-trash",
+            buttons: {
+                confirm: {
+                    text: 'Sim',
+                    btnClass: 'btn-dark',
+                    action: function () {
+                        $.ajax({
+                            type: "DELETE",
+                            btnClass: 'btn-dark',
+                            success: function () {
+                                $.alert({
+                                    title: "Produto excluido",
+                                    content: null,
+                                    icon: "fa fa-trash black-text",
+                                    theme: "modern"
+                                });
+                            }
+                        });
+                    }
+                },
+                cancel: {
+                    text: 'Não',
+                }
+            }
+        });
+
+    });
+
+});
+
+function setConteudoPaginaClicada(paginaASerCarregada) {
     let main = $("main")
-    main.html("")   
+    main.html("")
     main.load(paginaASerCarregada + ".html")
 }
 
